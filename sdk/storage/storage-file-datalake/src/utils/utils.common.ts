c49112eff68b871106f14d87c20a2a7c19a3a37a
Emma Zhu <emmazhu@microsoft.com>
2024-04-07 11:30:58 +0800

    [storage-file-datalake] Added ability to retrieve path ACL with DataLakePathClient.getProperties(), DataLakeFileClient.getProperties(), DataLakeDirectoryClient.getProperties(), DataLakeFileClient.read().

--- sdk/storage/storage-file-datalake/src/utils/utils.common.ts
+++ sdk/storage/storage-file-datalake/src/utils/utils.common.ts
@@ -3,7 +3,12 @@
 import { AbortSignalLike } from "@azure/abort-controller";
 import { HttpHeaders, HttpHeadersLike, HttpResponse, isNode, URLBuilder } from "@azure/core-http";
 import { ContainerEncryptionScope } from "@azure/storage-blob";
-import { CpkInfo, FileSystemEncryptionScope, PathPermissions } from "../models";
+import {
+  CpkInfo,
+  FileSystemEncryptionScope,
+  PathAccessControlItem,
+  PathPermissions,
+} from "../models";
 
 import {
   DevelopmentConnectionString,
@@ -12,7 +17,7 @@ import {
   PathStylePorts,
   UrlConstants,
 } from "./constants";
-import { toPermissions } from "../transforms";
+import { toAcl, toPermissions } from "../transforms";
 
 /**
  * Reserved URL characters must be properly escaped for Storage services like Blob or File.
@@ -650,12 +655,14 @@ export interface PathGetPropertiesRawResponseWithExtraPropertiesLike {
   owner?: string;
   group?: string;
   permissions?: PathPermissions;
+  acl: PathAccessControlItem[];
   _response: HttpResponse & {
     parsedHeaders: {
       encryptionContext?: string;
       owner?: string;
       group?: string;
       permissions?: PathPermissions;
+      acl: PathAccessControlItem[];
     };
   };
 }
@@ -685,11 +692,13 @@ export function ParsePathGetPropertiesExtraHeaderValues(
   response.owner = ParseHeaderValue(rawResponse, "x-ms-owner");
   response.group = ParseHeaderValue(rawResponse, "x-ms-group");
   response.permissions = toPermissions(ParseHeaderValue(rawResponse, "x-ms-permissions"));
+  response.acl = toAcl(ParseHeaderValue(rawResponse, "x-ms-acl"));
   if (response._response?.parsedHeaders) {
     response._response.parsedHeaders.encryptionContext = response.encryptionContext;
     response._response.parsedHeaders.owner = response.owner;
     response._response.parsedHeaders.group = response.group;
     response._response.parsedHeaders.permissions = response.permissions;
+    response._response.parsedHeaders.acl = response.acl;
   }
   return response;
 }
